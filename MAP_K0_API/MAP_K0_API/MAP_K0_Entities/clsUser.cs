db0e1ab378d481109191b397dda545a103b932d2
Kanadista <s.forte2001@gmail.com>
2022-04-21 22:57:50 +0200

    First commit

---
+++ MAP_K0_API/MAP_K0_API/MAP_K0_Entities/clsUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAP_K0_Entities
+{
+    public class clsUser
+    {
+        public int id { get; set; }
+        public string nickName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public byte[] profilePic { get; set; }
+        public int level { get; set; }
+        public int levelxp { get; get; }
+    }
+}
---
+++ MAP_K0_API/MAP_K0_API/MAP_K0_Entities/clsUserRatingLocation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAP_K0_Entities
+{
+    public class clsUserRatingLocation
+    {
+        public int idUser { get; set; }
+        public int idLocation { get; set; }
+        public int stars { get; set; }
+        public string comment { get; set; }
+    }
+}
